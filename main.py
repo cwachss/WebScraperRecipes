4534a7ba3ba421471f095d6b97d7f1865ec9b278
Chani Wachsstock <chaniwachsstock@gmail.com>
2022-06-19 09:08:21 +0300

    web scrape in progress

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # testing testing repository
 # this is the UI code if you can call the console a UI. I suppose it is.
+# sample url https://www.allrecipes.com/search/results/?search=chocolate+chip+cookies
+
+import pantry_processor
+import web_scraper
+
--- web_scraper.py
+++ web_scraper.py
@@ -1,3 +1,38 @@
 # here we shall scrape the webpage for ingredients list and for instructions. honestly I don't even want the pantry
 # matcher in my life i want a skip all the annoying blog info and just give me the recipe in a pretty format
 # oh also this will include the click on recipes functionality
+# class name that contains the link class="card__titleLink manual-link-behavior elementFont__titleLink margin-8-bottom"
+# once you get to the webpage, find the array called recipe ingredient
+
+import urllib.request
+import re
+
+
+def open_allrecipes():
+    recipe_search = input("enter recipe you would like to make")
+    recipe_search_updated = "https://www.allrecipes.com/search/results/?search=" + recipe_search.replace(" ", "+")
+    f = urllib.request.urlopen(recipe_search_updated)
+    data = f.read().decode()
+
+    return data
+
+
+def open_recipe_from_search_page(data):
+    search_for_string = 'class="card__titleLink manual-link-behavior elementFont__title margin-8-bottom"'
+    the_url_should_be_in_here = re.compile("search_for_string\n.*\nhref=(.*)\n", re.DOTALL)
+    return re.findall(the_url_should_be_in_here, data)
+
+
+def collect_ingredients(recipe_webpage):
+    f = urllib.request.urlopen(recipe_webpage)
+    data = f.read().decode()
+    ingredient_finder = re.compile('(recipeIngredient.*],)', re.DOTALL)
+    m = re.search(ingredient_finder, data)
+    ingredient_array = m.groups()
+    return ingredient_array
+
+
+webpage = open_allrecipes()
+#
+print(open_recipe_from_search_page(webpage))
+print(collect_ingredients("https://www.allrecipes.com/recipe/25040/chocolate-chip-cookies-v/"))
