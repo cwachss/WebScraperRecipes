de8a227faccf8c8dc138638feaba261448abd9b3
Chani Wachsstock <chaniwachsstock@gmail.com>
2022-06-20 17:37:46 +0300

    collected all recipes' ingredients on each page in array- this is not permanent. should break if recipe found.

--- web_scraper.py
+++ web_scraper.py
@@ -9,7 +9,7 @@ import re
 
 
 def open_allrecipes():
-    recipe_search = "chocolate chip cookies" #input("enter recipe you would like to make")
+    recipe_search = "chocolate chip cookies"  # input("enter recipe you would like to make")
     recipe_search_updated = "https://www.allrecipes.com/search/results/?search=" + recipe_search.replace(" ", "+")
     f = urllib.request.urlopen(recipe_search_updated)
     data = f.read().decode()
@@ -21,6 +21,7 @@ def open_recipe_from_search_page(data):
     search_for_string = 'class="card__titleLink manual-link-behavior elementFont__title margin-8-bottom"'
     the_url_should_be_in_here = re.compile(r'<a class="card__titleLink manual-link-behavior elementFont__titleLink '
                                            r'margin-8-bottom"\n\s*.*\n\s*href=(.*)\n[^>]*')
+    url = re.findall(the_url_should_be_in_here, data)
     return re.findall(the_url_should_be_in_here, data)
 
 
@@ -31,19 +32,27 @@ def collect_ingredients_and_instructions(recipe_webpage):
     m = re.search(ingredient_finder, data)
     recipe = m.groups()
     recipe_divided = recipe[0].split("recipeInstruction")
+
     # but maybe we should keep it because we may need it later, and it's a rather small file anyway
+    # we should definitely keep it. We'll deal with that later though - maybe a separate method
+    # called divide to temporarily divide it for the checker portion?
     ingredient_array = recipe_divided[0]
+
     return ingredient_array
 
 
 def look_for_matching_recipe():
     recipe_search = open_allrecipes()
     recipe_results = open_recipe_from_search_page(recipe_search)
-    for recipe in recipe_results():
-        ingredients = collect_ingredients_and_instructions(recipe)
+    for recipe in recipe_results:
+        recipe_minus_extra_quotes = recipe.replace('"', '')
+        # save current link! associated with the recipe and it's instructions!
+        ingredients = collect_ingredients_and_instructions(recipe_minus_extra_quotes)
+        print(ingredients)
         # do the matching thing on the ingredients half to the
+
 
 webpage = open_allrecipes()
 #
-print(open_recipe_from_search_page(webpage))
+look_for_matching_recipe()
 # print(collect_ingredients_and_instructions("https://www.allrecipes.com/recipe/25040/chocolate-chip-cookies-v/"))
