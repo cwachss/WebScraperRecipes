c028bedf744852900d96977314df5eba0d155ac8
Chani Wachsstock <chaniwachsstock@gmail.com>
2022-06-20 17:52:35 +0300

    add a dictionary of common substitutes so recipes are not crossed off the list because of minor differences

---
+++ ingredient_dictionary.py
@@ -0,0 +1,10 @@
+ingredient_substitutes = {
+    "butter": "margarine",
+    "vanilla extract": "vanilla sugar",
+    "milk": "almond milk",
+    "chocolate chunks": "chocolate chips",
+    "powdered sugar": "confectioner's sugar",
+    "icing sugar": "powdered sugar",
+    "confectioner's sugar": "icing sugar"
+
+}
